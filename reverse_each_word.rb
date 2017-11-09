de5a5a235f3bfd0576f1146d4ed3a5067f2dc2c8
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 16:34:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence1)
-  words = sentence1.split
-  words.each do |word|
-    word.reverse
-  end
+  sentence1.reverse
+  # words = sentence1.split
+  # words.each do |word|
+  #   word.reverse
+  # end
   return words.join(' ')
 end
